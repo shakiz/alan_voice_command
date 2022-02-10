14be24beb1c506730c1aeaad68bc74afc9059994
shakiz <activities335@gmail.com>
2022-02-11 01:00:34 +0600

    alan integrated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:alan_voice/alan_voice.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,11 +10,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    /// Init Alan Button with project key from Alan Studio
+    AlanVoice.addButton(
+        "502ebcc9f884646444d4ab00d0618b172e956eca572e1d8b807a3e2338fdd0dc/stage",
+        buttonAlign: AlanVoice.BUTTON_ALIGN_LEFT);
+
+    /// Handle commands from Alan Studio
+    AlanVoice.onCommand.add((command) {
+      debugPrint("got new command ${command.toString()}");
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Alan AI"),
+          leading: InkWell(
+            child: const IconTheme(
+                data: IconThemeData(color: Colors.white),
+                child: Icon(
+                  Icons.menu,
+                )),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {},
+        ),
         body: body(),
       ),
     );
@@ -23,13 +53,12 @@ class _HomePageState extends State<HomePage> {
     return SingleChildScrollView(
       child: Container(
         width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         padding: const EdgeInsets.all(24),
-        color: Colors.black12.withOpacity(0.01),
+        color: Colors.white,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-
-          ],
+          children: [],
         ),
       ),
     );
