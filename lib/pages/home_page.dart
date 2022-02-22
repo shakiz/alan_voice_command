45c40cc2fa33dc40221f72fe79fdbc1af2cdce1c
shakiz <activities335@gmail.com>
2022-02-22 20:50:31 +0600

    result processing test

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List greetingsText = ['hi', 'hello', 'thanks', 'welcome'];
+  List cancelText = ['cancel', 'close', 'remove', 'delete'];
+
   @override
   void initState() {
     /// Init Alan Button with project key from Alan Studio
@@ -64,15 +67,24 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  /// Responsible for handling commands sent from the Alan voice script.
+  /// To accompany user’s utterances with activities in the app UI, you can send commands
+  /// from the voice scripts to the client app. For example, once the user gives a voice command, another view or
+  /// page can be opened in the app, a UI element can be selected on the screen and so on.
   handleCommand(Map<String, dynamic> command){
     switch(command["command"]){
       default:
-        print("Done");
+        debugPrint("Done");
         break;
     }
   }
 
   /// Handling events
+  /// Responsible for handling events received from Alan.
+  /// While Alan interacts with the user and interpretes the user’s input, it emits a series of events.
+  /// For each event, Alan generates JSON output with the event description. You can intercept the event information and
+  /// use it in your app logic if needed.
+  /// https://alan.app/docs/client-api/methods/event-handler/
   void _handleEvent(Map<String, dynamic> event) {
     switch (event["name"]) {
       case "recognized":
@@ -80,6 +92,12 @@ class _HomePageState extends State<HomePage> {
         break;
       case "parsed":
         debugPrint("Final result: ${event["text"]}");
+        if(greetingsText.contains(event["text"])){
+          _showMyDialog();
+        }
+        else if(cancelText.contains(event["text"])){
+          Navigator.pop(context);
+        }
         break;
       case "text":
         debugPrint("Alan response: ${event["text"]}");
@@ -87,5 +105,33 @@ class _HomePageState extends State<HomePage> {
       default:
         debugPrint("Unknown event");
     }
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('This is a demo alert dialog.'),
+                Text('Would you like to approve of this message?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Approve'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
